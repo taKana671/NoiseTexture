3798069448a184c86e022147386c467fa7dfa221
taKana671 <kanae5321@gmail.com>
2024-11-04 11:15:56 +0900

    Changed image file name to be output.

--- create_image.py
+++ create_image.py
@@ -6,7 +6,7 @@ import numpy as np
 
 def create_image_8bit(arr, ext='png'):
     now = datetime.now()
-    file_name = f'image_{now.strftime("%Y%m%d%H%M%S")}.{ext}'
+    file_name = f'image8_{now.strftime("%Y%m%d%H%M%S")}.{ext}'
 
     arr = np.clip(arr * 255, a_min=0, a_max=255).astype(np.uint8)
     cv2.imwrite(file_name, arr)
@@ -14,7 +14,7 @@ def create_image_8bit(arr, ext='png'):
 
 def create_image_16bit(arr):
     now = datetime.now()
-    file_name = f'image_{now.strftime("%Y%m%d%H%M%S")}.png'
+    file_name = f'image16_{now.strftime("%Y%m%d%H%M%S")}.png'
 
     arr = np.clip(arr * 65535, a_min=0, a_max=65535).astype(np.uint16)
     cv2.imwrite(file_name, arr)
