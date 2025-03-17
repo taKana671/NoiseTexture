1fed4aef666c1484da124f841d407e774ae142b0
taKana671 <kanae5321@gmail.com>
2025-03-17 19:15:28 +0900

    Changed simplex noise.

--- pynoise/simplex.py
+++ pynoise/simplex.py
@@ -6,11 +6,15 @@ from pynoise.noise import Noise
 
 class Simplex(Noise):
 
-    def __init__(self, width=256, height=256, space_scale=20):
-        # self.size = 256
-        self.space_scale = space_scale
-        self.width = width
-        self.height = height
+    # def __init__(self, weight=0.5, lacunarity=2.01, octaves=4,
+    #              width=256, height=256, space_scale=20):
+    # def __init__(self, width=256, height=256, space_scale=20):
+    #     # self.weight = weight
+    #     # self.lacunarity = lacunarity
+    #     # self.octaves = octaves
+    #     self.scale = space_scale
+    #     self.width = width
+    #     self.height = height
 
     def mod289(self, p):
         return p - np.floor(p * (1.0 / 289.0)) * 289.0
@@ -45,10 +49,15 @@ class Simplex(Noise):
 
         # Do some permutations to avoid truncation effects in permutation
         i = self.mod289(i)
-        perm = self.permute(i[1] + np.array([0.0, i1[1], 1.0]))
-        perm = self.permute(perm + i[0] + np.array([0.0, i1[0], 1.0]))
+        perm = 0
 
-        v = np.array([np.dot(x0, x0), np.dot(x1, x1), np.dot(x2, x2)])
+        for idx in range(1, -1, -1):
+            perm = self.permute(perm + i[idx] + np.array([0.0, i1[idx], 1.0]))
+
+        # perm = self.permute(i[1] + np.array([0.0, i1[1], 1.0]))
+        # perm = self.permute(perm + i[0] + np.array([0.0, i1[0], 1.0]))
+
+        v = np.array([np.dot(c, c) for c in (x0, x1, x2)])
         m = np.maximum(0.5 - v, 0.0)
         m = m ** 4
 
@@ -78,7 +87,7 @@ class Simplex(Noise):
         x0 = p - i + np.dot(i, np.repeat(c[0], 3))
 
         # other corners
-        g = np.array([self.step(v1, v2) for v1, v2 in zip(x0[[1, 2, 0]], x0)])
+        g = self.step_arr(x0[[1, 2, 0]], x0)
         ll = 1.0 - g
         i1 = np.minimum(g, ll[[2, 0, 1]])
         i2 = np.maximum(g, ll[[2, 0, 1]])
@@ -89,9 +98,14 @@ class Simplex(Noise):
 
         # Do some permutations to avoid truncation effects in permutation
         i = self.mod289(i)
-        perm = self.permute(i[2] + np.array([0.0, i1[2], i2[2], 1.0]))
-        perm = self.permute(perm + i[1] + np.array([0.0, i1[1], i2[1], 1.0]))
-        perm = self.permute(perm + i[0] + np.array([0.0, i1[0], i2[0], 1.0]))
+        perm = 0
+
+        for idx in range(2, -1, -1):
+            perm = self.permute(perm + i[idx] + np.array([0.0, i1[idx], i2[idx], 1.0]))
+
+        # perm = self.permute(i[2] + np.array([0.0, i1[2], i2[2], 1.0]))
+        # perm = self.permute(perm + i[1] + np.array([0.0, i1[1], i2[1], 1.0]))
+        # perm = self.permute(perm + i[0] + np.array([0.0, i1[0], i2[0], 1.0]))
 
         # Gradients: 7x7 points over a square, mapped onto an octahedron.
         # The ring size 17*17 = 289 is close to a multiple of 49 (49*6 = 294)
@@ -101,6 +115,7 @@ class Simplex(Noise):
         _x = np.floor(j * ns[2])
         _y = np.floor(j - 7.0 * _x)
         x = _x * ns[0] + np.repeat(ns[1], 4)
+
         y = _y * ns[0] + np.repeat(ns[1], 4)
         h = 1.0 - np.abs(x) - np.abs(y)
 
@@ -108,7 +123,7 @@ class Simplex(Noise):
         b1 = np.concatenate([x[2:], y[2:]])
         s0 = np.floor(b0) * 2.0 + 1.0
         s1 = np.floor(b1) * 2.0 + 1.0
-        sh = np.array([self.step(v, 0) for v in h]) * -1
+        sh = self.step_arr(h, [0] * 4) * -1
         a0 = b0[[0, 2, 1, 3]] + s0[[0, 2, 1, 3]] * sh[[0, 0, 1, 1]]
         a1 = b1[[0, 2, 1, 3]] + s1[[0, 2, 1, 3]] * sh[[2, 2, 3, 3]]
 
@@ -118,155 +133,91 @@ class Simplex(Noise):
         p3 = np.concatenate([a1[2:], h[3:]])
 
         # normalize gradients
-        nm = self.inverssqrt(
-            np.array([np.dot(p0, p0), np.dot(p1, p1), np.dot(p2, p2), np.dot(p3, p3)]))
+        pts = (p0, p1, p2, p3)
+        corners = (x0, x1, x2, x3)
+
+        nm = self.inverssqrt(np.array([np.dot(pt, pt) for pt in pts]))
         p0 *= nm[0]
         p1 *= nm[1]
         p2 *= nm[2]
         p3 *= nm[3]
 
-        arr = np.array([np.dot(x0, x0), np.dot(x1, x1), np.dot(x2, x2), np.dot(x3, x3)])
+        arr = np.array([np.dot(co, co) for co in corners])
         m = np.maximum(0.6 - arr, 0)
-        m = m ** 4
+        # m = m ** 4
 
-        arr = np.array([np.dot(p0, x0), np.dot(p1, x1), np.dot(p2, x2), np.dot(p3, x3)])
-        return 42.0 * np.dot(m, arr) * 0.5 + 0.5
+        arr = np.array([np.dot(pt, co) for pt, co in zip(pts, corners)])
+        # return 42.0 * np.dot(m, arr) * 0.5 + 0.5
+        return 42.0 * np.dot(m ** 4, arr) * 0.5 + 0.5
 
-    def noise2(self, t=None):
-        t = self.mock_time() if t is None else t
-        m = min(self.width, self.height)
-
-        arr = np.array(
-            [self.snoise2(np.array([x, y]) + t / m * self.space_scale)
-                for y in range(self.height)
-                for x in range(self.width)]
-        )
-
-        arr = arr.reshape(self.height, self.width)
-        # arr = np.clip(arr * 255, a_min=0, a_max=255).astype(np.uint8)
-        # cv2.imwrite('simplex.png', arr)
-        return arr
-
-    def noise3(self, t=None):
-        t = self.mock_time() if t is None else t
-        m = min(self.width, self.height)
-
-        arr = np.array(
-            [self.snoise3(np.array([x, y, t]) / m * self.space_scale)
-                for y in range(self.height)
-                for x in range(self.width)]
-        )
-
-        arr = arr.reshape(self.height, self.width)
-        # arr = np.clip(arr * 255, a_min=0, a_max=255).astype(np.uint8)
-        # cv2.imwrite('simplex.png', arr)
-        return arr
-
-
-
-
-
-
-
-    # def gtable2(self, lattice, p):
-    #     lattice = lattice.astype(np.uint32)
-
-    #     if (tup := tuple(lattice)) in self.hash:
-    #         idx = self.hash[tup]
-    #     else:
-    #         self.uhash22(lattice)
-    #         idx = lattice[0] >> 29
-    #         self.hash[tup] = idx
-
-    #     u = (p[0] if idx < 4 else p[1]) * 0.92387953   # 0.92387953 = cos(pi/8)
-    #     v = (p[1] if idx < 4 else p[0]) * 0.38268343   # 0.38268343 = sin(pi/8)
-
-    #     _u = u if idx & 1 == 0 else -u
-    #     _v = v if idx & 2 == 0 else -v
-
-    #     return _u + _v
-
-    # def gtable3(self, lattice, p):
-    #     lattice = lattice.astype(np.uint32)
-
-    #     if (tup := tuple(lattice)) in self.hash:
-    #         idx = self.hash[tup]
-    #     else:
-    #         self.uhash33(lattice)
-    #         idx = lattice[0] >> 28
-    #         self.hash[tup] = idx
-
-    #     u = p[0] if idx < 8 else p[1]
-
-    #     if idx < 4:
-    #         v = p[1]
-    #     elif idx == 12 or idx == 14:
-    #         v = p[0]
-    #     else:
-    #         v = p[2]
-
-    #     _u = u if idx & 1 == 0 else -u
-    #     _v = v if idx & 2 == 0 else -v
-
-    #     return _u + _v
-
-    # def pnoise2(self, p):
-    #     n = np.floor(p)
-    #     f, _ = np.modf(p)
-
-    #     v = [self.gtable2(n + (arr := np.array([i, j])), f - arr)
-    #          for j in range(2) for i in range(2)]
-
-    #     f = self.fade(f)
-    #     w0 = self.mix(v[0], v[1], f[0])
-    #     w1 = self.mix(v[2], v[3], f[0])
-    #     return 0.5 * self.mix(w0, w1, f[1]) + 0.5
-
-    # def pnoise3(self, p):
-    #     n = np.floor(p)
-    #     f, _ = np.modf(p)
-
-    #     # 0.70710678 = 1 / sqrt(2)
-    #     v = [self.gtable3(n + (arr := np.array([i, j, k])), f - arr) * 0.70710678
-    #          for k in range(2) for j in range(2) for i in range(2)]
-
-    #     f = self.fade(f)
-    #     w0 = self.mix(self.mix(v[0], v[1], f[0]), self.mix(v[2], v[3], f[0]), f[1])
-    #     w1 = self.mix(self.mix(v[4], v[5], f[0]), self.mix(v[6], v[7], f[0]), f[1])
-    #     return 0.5 * self.mix(w0, w1, f[2]) + 0.5
-
-    # def noise2(self, t=None):
-    #     t = self.mock_time() if t is None else t
-    #     self.hash = {}
-
-    #     arr = np.array(
-    #         [self.pnoise2(np.array([x + t, y + t]))
-    #             for y in np.linspace(0, self.grid, self.size)
-    #             for x in np.linspace(0, self.grid, self.size)]
-    #     )
-    #     arr = arr.reshape(self.size, self.size)
-    #     return arr
-
-    # def noise3(self, t=None):
-    #     t = self.mock_time() if t is None else t
-    #     self.hash = {}
-
-    #     arr = np.array(
-    #         [self.pnoise3(np.array([x + t, y + t, t]))
-    #             for y in np.linspace(0, self.grid, self.size)
-    #             for x in np.linspace(0, self.grid, self.size)]
-    #     )
-    #     arr = arr.reshape(self.size, self.size)
-    #     return arr
-
-    # def wrap2(self, x, y, rot=False):
+    # def fractal(self, noise, p):
+    #     """Args:
+    #         noise (callable): a function to generate noise.
+    #         p (Numpy.ndarary): point
+    #     """
     #     v = 0.0
+    #     amp = 1.0
+    #     freq = 1.0
 
-    #     for _ in range(4):
-    #         cx = np.cos(2 * np.pi * v) if rot else v
-    #         sy = np.sin(2 * np.pi * v) if rot else v
-    #         _x = x + self.weight * cx
-    #         _y = y + self.weight * sy
-    #         v = self.pnoise2(np.array([_x, _y]))
+    #     for _ in range(self.octaves):
+    #         v += amp * (noise(freq * p) - 0.5)
+    #         amp *= self.weight
+    #         freq *= self.lacunarity
 
-    #     return v
+    #     return 0.5 * v + 0.5
+
+    def noise2(self, width=256, height=256, scale=20, t=None):
+        t = self.mock_time() if t is None else t
+        m = min(self.width, self.height)
+
+        # arr = np.array(
+        #     [self.snoise2((np.array([x + t, y + t]) / m) * self.scale)
+        #         for y in range(self.height)
+        #         for x in range(self.width)]
+        # )
+
+        arr = np.array(
+            [self.snoise2((np.array([x, y]) / m + t) * scale)
+                for y in range(height)
+                for x in range(width)]
+        )
+
+        arr = arr.reshape(self.height, self.width)
+        # arr = np.clip(arr * 255, a_min=0, a_max=255).astype(np.uint8)
+        # cv2.imwrite('simplex.png', arr)
+        return arr
+
+    def noise3(self, width=256, height=256, scale=20, t=None):
+        t = self.mock_time() if t is None else t
+        m = min(self.width, self.height)
+
+        # arr = np.array(
+        #     [self.snoise3((np.array([x / m, y / m, t])) * self.scale)
+        #         for y in range(self.height)
+        #         for x in range(self.width)]
+        # )
+        arr = np.array(
+            [self.snoise3((np.array([x / m + t, y / m + t, t])) * scale)
+                for y in range(height)
+                for x in range(width)]
+        )
+
+        arr = arr.reshape(self.height, self.width)
+        # arr = np.clip(arr * 255, a_min=0, a_max=255).astype(np.uint8)
+        # cv2.imwrite('simplex.png', arr)
+        return arr
+
+    # def noise3_fractal(self, t=None):
+    #     t = self.mock_time() if t is None else t
+    #     m = min(self.width, self.height)
+
+    #     arr = np.array(
+    #         [self.fractal((np.array([x / m, y / m, t])) * self.scale)
+    #             for y in range(self.height)
+    #             for x in range(self.width)]
+    #     )
+
+    #     arr = arr.reshape(self.height, self.width)
+    #     # arr = np.clip(arr * 255, a_min=0, a_max=255).astype(np.uint8)
+    #     # cv2.imwrite('simplex.png', arr)
+    #     return arr
