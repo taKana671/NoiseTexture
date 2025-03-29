b70b52f2a1f0f978766765966ea22276b58f8bff
taKana671 <kanae5321@gmail.com>
2025-03-30 00:53:06 +0900

    Changed setup.py.

--- output_image.py
+++ output_image.py
@@ -12,12 +12,12 @@ def make_dir(name, parent='.'):
     return path
 
 
-def output(arr, stem, parent='.', ext='png', with_suffix=True):
+def output(arr, stem, parent='.', with_suffix=True):
     if with_suffix:
         now = datetime.now()
         stem = f'{stem}_{now.strftime("%Y%m%d%H%M%S")}'
 
-    file_path = f'{parent}/{stem}.{ext}'
+    file_path = f'{parent}/{stem}.png'
     # file_name = f'{stem}_{now.strftime("%Y%m%d%H%M%S")}.{ext}'
     cv2.imwrite(file_path, arr)
 
